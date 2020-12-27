24a9b5b38f9a3bbdc89f12c20f39c9ed73d9ac60
jose <joyjacky94212@gmail.com>
2020-12-27 19:45:23 +0530

    Selenium Portal: udpating drivers

--- SeleniumPortal/src/main/java/com/flipkart/services/InitiateDriverBrowser.java
+++ SeleniumPortal/src/main/java/com/flipkart/services/InitiateDriverBrowser.java
@@ -18,8 +18,14 @@ public class InitiateDriverBrowser {
 	public static WebDriver initiateDriverBrowser(String browser, String url) {
 		try {
 			if (browser.toLowerCase().contains("chrome")) {
-				System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
-				driver = new ChromeDriver();
+				String OS = System.getProperty("os.name");
+				if (OS.equalsIgnoreCase("linux")) {
+					System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
+					driver = new ChromeDriver();
+				} else {
+					System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+					driver = new ChromeDriver();
+				}
 				log.info("initialized driver and browser:" + browser);
 				driver.get(url);
 				log.info("fetched url:" + url);
